b73e26f59be2b468f0e3c618e6bebd4eee1dd152
Jr3582 <jr3582@drexel.edu>
2025-08-13 19:05:51 -0400

    initial set up

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+export const Home = () => {
+    return (
+    <div className="min-h-screen">
+        {/* Navbar */}
+
+        {/* Main Content */}
+        <div>
+            {/* Pr Page */}
+
+            {/* Calorie Intake*/}
+
+        </div>
+
+        {/* Footer */}
+
+    </div>
+    )
+}
